3e1f7187937954ea5de9c3ec7664003a2f252423
Mustapha-AJEGHRIR <m5u9s00@gmail.com>
2022-02-13 08:47:26 +0100

    Implemented thed CRNN module

--- src/load_dataset_fft.py
+++ src/load_dataset_fft.py
@@ -1,5 +1,6 @@
 #%%
 # -------------------------------- torch stuff ------------------------------- #
+from curses import window
 import torch
 from torch.utils.data import Dataset, DataLoader
 
@@ -11,7 +12,9 @@ import json
 import os
 from tqdm import tqdm
 # import sys
-# import 
+import librosa
+import scipy
+import numpy as np
 
 # ---------------------------------------------------------------------------- #
 #                                   Constants                                  #
@@ -31,35 +34,21 @@ data_dir = os.path.join(os.path.dirname(__file__),"../data/LibriCount")
 
 
 
-class AudioCountGender(Dataset):
-    def __init__(self, data_dir=data_dir, dtype = FTYPE, cache=True, **kwargs):
+class AudioCountGenderFft(Dataset):
+    def __init__(self, data_dir=data_dir,
+                dtype = FTYPE,
+                fft_nperseg = 400,   # from the paper
+                fft_noverlap = 240,  # from the paper
+                fft_window_type = "tukey", # default
+                fft_in_db = False,
+                **kwargs):
         self.sounds = glob(os.path.join(data_dir,"*.wav"))
         self.labels = glob(os.path.join(data_dir,"*.json"))
         self.dtype = dtype
-        self.cache = cache
-        if self.cache:
-            self.data = []
-            for index in tqdm(range(len(self.sounds)), "Caching dataset"):
-                sample_rate, clip = wavfile.read(self.sounds[index])
-                # assert self.sounds[index].split(".")[:-1] == self.labels[index].split(".")[:-1], "Sound and label files do not match in the order"            
-                with open(self.labels[index]) as f:
-                    label = json.load(f)
-                genders = [0, 0] #[Male, Female]
-                for person in label:
-                    gender = person["sex"]
-                    if gender == "F":
-                        genders[1] += 1
-                    else :
-                        genders[0] += 1
-                self.data.append([torch.tensor(clip, dtype=self.dtype).unsqueeze(0), torch.tensor(genders, dtype=self.dtype)]) #unsqueeze serves for channel = 1
-                
-                
-    def __getitem__(self, index):
-        if self.cache:
-            return self.data[index]
-        else:
-            # clip, sample_rate = sf.read(self.sounds[index])
+        self.data = []
+        for index in tqdm(range(len(self.sounds)), "Caching dataset"):
             sample_rate, clip = wavfile.read(self.sounds[index])
+            # assert self.sounds[index].split(".")[:-1] == self.labels[index].split(".")[:-1], "Sound and label files do not match in the order"            
             with open(self.labels[index]) as f:
                 label = json.load(f)
             genders = [0, 0] #[Male, Female]
@@ -69,18 +58,37 @@ class AudioCountGender(Dataset):
                     genders[1] += 1
                 else :
                     genders[0] += 1
-            return torch.tensor(clip, dtype=self.dtype).unsqueeze(0), torch.tensor(genders, dtype=self.dtype) #unsqueeze serves for channel = 1
+                    
+            _, _, fft = scipy.signal.spectrogram(clip,
+                                                    fs = sample_rate,
+                                                    nperseg = fft_nperseg,
+                                                    noverlap = fft_noverlap,
+                                                    window = fft_window_type
+                                                    )
+            fft /= np.linalg.norm(fft, axis=0, keepdims=True)
+            if fft_in_db:
+                fft = librosa.amplitude_to_db(fft, ref=np.max)
+            
+            self.data.append([torch.tensor(fft, dtype=self.dtype).unsqueeze(0), torch.tensor(genders, dtype=self.dtype)]) #unsqueeze serves for channel = 1
+                
+                
+    def __getitem__(self, index):
+        return self.data[index]
     def __len__(self):
         return len(self.sounds)
 
 
-def get_splitter_dataloaders(**kwargs):
+def get_splitter_dataloaders_fft(**kwargs):
     """Return dataloaders for both training and validation sets,
     kmwargs could include:
         BATCH_SIZE
         TRAIN_SPLIT
         FTYPE
-
+        fft_nperseg
+        fft_noverlap
+        fft_window_type
+        fft_in_db
+        
     Returns:
         Dataloader: train_loader
         Dataloader: val_loader
@@ -95,7 +103,7 @@ def get_splitter_dataloaders(**kwargs):
     else:
         train_split = TRAIN_SPLIT
         
-    data = AudioCountGender(**kwargs)
+    data = AudioCountGenderFft(**kwargs)
     train, val = torch.utils.data.random_split(data, [int(len(data)*train_split), len(data)-int(len(data)*train_split)])
     train_loader = DataLoader(dataset=train, batch_size=batch_size, shuffle=True, num_workers=4)
     val_loader = DataLoader(dataset=val, batch_size=batch_size, shuffle=True, num_workers=4)
