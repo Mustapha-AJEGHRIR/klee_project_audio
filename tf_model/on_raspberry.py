1c5fa008a49021bfb9a1a0a2c315cefdde527c56
Mustapha-AJEGHRIR <m5u9s00@gmail.com>
2022-04-06 15:24:45 +0200

    ready to be in Raspberry

--- tf_model/on_raspberry.py
+++ tf_model/on_raspberry.py
@@ -14,24 +14,38 @@ FFT_N_PERSEG = 400
 FFT_N_OVERLAP = 240
 FFT_WINDOW_TYPE = "tukey"
 EPS = 1e-8
-SAMPLE_RATE = 16000
+SAMPLE_RATE = 16000 # sample rate
+SECONDS = 5  # Duration of recording
 
-ort_sess = ort.InferenceSession('onnx/f-crnn.onnx')
+# ---------------------------------------------------------------------------- #
+#                                    Models                                    #
+# ---------------------------------------------------------------------------- #
+
+gender_count_sess = ort.InferenceSession('onnx/f-crnn.onnx')
+people_counter_sess = ort.InferenceSession('../old_model_conversion/onnx/CRNN_ONNX.onnx')
+input_names = {
+    "p" : "zero1_input",
+    "g" : "x",
+}
+
+ort_sess = people_counter_sess
+model = "p"
 
 while True :
     # ---------------------------------------------------------------------------- #
     #                                 Record Audio                                 #
     # ---------------------------------------------------------------------------- #
-    fs = SAMPLE_RATE  # Sample rate
-    seconds = 5  # Duration of recording
     print("Recording ...")
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+    tik = time()
+    myrecording = sd.rec(int(SECONDS * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=1)
     sd.wait()  # Wait until recording is finished
     myrecording = myrecording[:,0]
+    tok = time()
+    print("Recording done in %.2f seconds" % (tok - tik))
 
 
 
-    write('tmp/audio_recorded.wav', fs, myrecording)  # Save as WAV file 
+    write('tmp/audio_recorded.wav', SAMPLE_RATE, myrecording)  # Save as WAV file 
 
     # ---------------------------------------------------------------------------- #
     #                               Preprocess Audio                               #
@@ -47,7 +61,11 @@ while True :
     fft_rec = fft_rec / (np.linalg.norm(fft_rec, axis=0, keepdims=True) + EPS)
     fft_rec = fft_rec.astype(np.float32)
     fft_rec = fft_rec[None, :, :] # Add the batch dimension = 1
-    fft_rec.shape
+    if model == "p":
+        fft_rec = np.transpose(fft_rec, (0, 2, 1))
+        fft_rec = np.pad(fft_rec, ((0, 0), (0, 2), (0, 0)))
+        fft_rec = fft_rec[None, :, :, :]
+    
     tok = time()
     print("pre processing time : {:.3f}s".format(tok-tik))
 
@@ -57,13 +75,38 @@ while True :
     #                                   Inference                                  #
     # ---------------------------------------------------------------------------- #
     tik = time()
-    outputs = ort_sess.run([], {'x': fft_rec})
+    input_name = input_names[model]
+    outputs = ort_sess.run([], {input_name: fft_rec})
     tok = time()
-    print("Activations [M, F] :", outputs[0][0])
-    print("Total amount of people :", round(outputs[0][0].sum()))
-    print("Duration :{:.3f}s".format(tok-tik))
+    if model == "p":
+        print("\t=> Activations [M, F] :   ", outputs[0][0])
+        print("\t=> Total amount of people :   ", np.argmax(outputs[0][0]))
+        print("\t=> Duration :   {:.3f}s".format(tok-tik))
+    else:
+        print("\t=> Activations [M, F] :   ", outputs[0][0])
+        print("\t=> Total amount of people :   ", round(outputs[0][0].sum()))
+        print("\t=> Duration :   {:.3f}s".format(tok-tik))
 
 
-    con = input("continue ? enter y : ")
-    if con != "y":
+    
+    # ---------------------------------------------------------------------------- #
+    #                                     Menu                                     #
+    # ---------------------------------------------------------------------------- #
+    print("****")
+    print("q - Quit")
+    print("g - Gender counter")
+    print("p - People counter")
+    
+    ans = input("Enter : ")
+    if ans == "q":
         break
+    elif ans == "g":
+        ort_sess = gender_count_sess
+        model = ans
+    elif ans == "p":
+        model = ans
+        ort_sess = people_counter_sess
+    else :
+        print("Continuing with the same model ...")
+    
+    
